1ee42841c55f8fca9e1c9623058d18d25d2bcec7
linneaagetorp <linnea.agetorp@gmail.com>
2018-05-21 15:32:03 +0200

    v3

---
+++ v3/src/components/files.js
@@ -0,0 +1,17 @@
+
+// class Files ...
+
+// componentDidMount() {
+//     this.props.fetchFiles
+// }
+
+
+// render() {
+//     return (
+//         files
+//             ?
+//             <ul>
+//                 {files.map(file => <li>{file}</li>)}
+//             </ul>
+//             :
+//             <något annat>
--- v3/src/components/login.js
+++ v3/src/components/login.js
@@ -1,29 +1,29 @@
-import React from   'react';
-import {Fragment as F} from 'react';
+import React, {Fragment as F} from   'react';
 import {Dropbox} from 'dropbox';
+import utils from './index';
 
 
 export default class Login extends React.Component{
+    //client id = app key
+    CLIENT_ID = 'hamdg2o7t6tygwy'
 
-    // dbx = new Dropbox({ accessToken: 'KNeTyKvgwzAAAAAAAAAA2Ip29UxARWYwxU-_E1De_NaUp0ZPiTizRbEuQip6OX9o' });
-
+//renderItems ska utföras i FILES-filen
     renderItems = (items) => {
         console.log(items)
-        var filesContainer = document.getElementById('files');
-        items.forEach(function(item) {
-            var li = document.createElement('li');
-            li.innerHTML = item.name;
-            filesContainer.appendChild(li);
-        });
+        // const filesContainer = document.getElementById('files');
+        // items.forEach(function(item) {
+        //     const li = document.createElement('li');
+        //     li.innerHTML = item.name;
+        //     filesContainer.appendChild(li);
+        // });
     }
 
-    //access_token = 'KNeTyKvgwzAAAAAAAAAA2Ip29UxARWYwxU-_E1De_NaUp0ZPiTizRbEuQip6OX9o';
 
-    getAccessTokenFromUrl = () => {
 
-        return 'KNeTyKvgwzAAAAAAAAAA2Ip29UxARWYwxU-_E1De_NaUp0ZPiTizRbEuQip6OX9o';
-    }
 
+
+
+    //detta ska vara en action
     onClickHandler = () => {
         const that = this;
        let dbx = new Dropbox({ accessToken: this.getAccessTokenFromUrl() })
@@ -31,6 +31,7 @@ export default class Login extends React.Component{
         dbx.filesListFolder({path: ''})
             .then(function(response) {
                 console.log(that)
+
                 that.renderItems(response.entries);
             })
             .catch(function(error) {
@@ -38,31 +39,44 @@ export default class Login extends React.Component{
             });
     }
 
-    clickHandler = () => {
-        this.onClickHandler()
-        console.log('körs detta')
+    getAccessTokenFromUrl = () => {
+        return utils.parseQueryString(window.location.hash).accessToken;
+        // return 'KNeTyKvgwzAAAAAAAAAA2Ip29UxARWYwxU-_E1De_NaUp0ZPiTizRbEuQip6OX9o';
     }
+
+    isAuthenticated = () => {
+        return !!this.getAccessTokenFromUrl
+    }
+
 
 
     // componentDidMount(){
     //     if(isAuthenticated()) {
-    //
+    //var dbx = new Dropbox.Dropbox({ clientId: CLIENT_ID });
+    //     var authUrl = dbx.getAuthenticationUrl('http://localhost:8080/auth');
+    //     document.getElementById('authlink').href = authUrl;
+
     //     } else {
     //
+
     //     }
     // }
 
     render() {
         return (
+            this.isAuthenticated()
+            ?
             <F>
-                <input
+                <button>Log Out</button>
+                <p>
+                    {this.CLIENT_ID}
+                </p>
+                {/*<Files></Files>*/}
+            </F>
+            :
+            <F>
 
-                placeholder='username'
-                />
-                <input
-                placeholder='password'
-                />
-                <button onClick={this.clickHandler} >  CLICK to authenticate</button>
+                <button onClick={this.onClickHandler} >  CLICK to authenticate</button>
             </F>
         )
     }
